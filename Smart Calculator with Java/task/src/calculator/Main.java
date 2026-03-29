1b460c0a0d86e5b27766f14acc4e4f1c484dbb5b
salcines <salcines@outlook.es>
2026-03-30 00:46:53 +0200

    Smart Calculator. First Stage - Sum two given numbers and display

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -7,5 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+
+        add(firstNumber, secondNumber);
+    }
+    public static void add(int firstNumber, int secondNumber) {
+        System.out.println(firstNumber + secondNumber);
     }
 }
