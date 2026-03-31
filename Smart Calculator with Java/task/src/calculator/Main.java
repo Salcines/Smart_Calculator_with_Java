085c203fcbb2f650de00e21a06d6cfb706db1bc0
salcines <salcines@outlook.es>
2026-04-01 01:14:17 +0200

    Smart Calculator. Second Stage - Sum two given numbers until "/exit" is input.

--- Smart Calculator with Java/task/src/calculator/Main.java
+++ Smart Calculator with Java/task/src/calculator/Main.java
@@ -1,18 +1,38 @@
 package calculator;
 
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        final String EXIT_COMMAND = "/exit";
+
         Scanner scanner = new Scanner(System.in);
         // put your code here
-        int firstNumber = scanner.nextInt();
-        int secondNumber = scanner.nextInt();
+        while (true) {
 
-        add(firstNumber, secondNumber);
+            String line = scanner.nextLine();
+            if (line.equals(EXIT_COMMAND)) {
+                break;
+            }
+
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            String[] numbers = line.split("\\s+");
+            int firstNumber =Integer.parseInt(numbers[0]);
+            int secondNumber = numbers.length > 1 ? Integer.parseInt(numbers[1]) : 0;
+
+            add(firstNumber, secondNumber);
+        }
+        System.out.println("Bye!");
     }
-    public static void add(int firstNumber, int secondNumber) {
+
+    public static void add(int firstNumber,
+                           int secondNumber) {
         System.out.println(firstNumber + secondNumber);
     }
 }
