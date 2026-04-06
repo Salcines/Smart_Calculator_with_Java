3ea6482523074a53a55ced34a110feb73e8c9762
salcines <salcines@outlook.es>
2026-04-06 14:51:49 +0200

    For loop task 7 - The product of numbers from a to b

---
+++ Topics/For loop/The product of numbers from a to b/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+        int lowerLimit = scanner.nextInt();
+        int upperLimit = scanner.nextInt();
+        long result = 1;
+
+        for (int i = lowerLimit; i < upperLimit; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
